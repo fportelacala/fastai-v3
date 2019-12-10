b76a00b47d0b0a9f727dd01a3c539d0ee1e9dcfa
fportelacala <fportela66@hotmail.com>
2019-12-10 12:23:06 -0500

    update google drive share link

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/open?id=1H8uhTbnHLkRY2DGmnOWQtW9puddwPUT2'
+export_file_url = 'https://drive.google.com/open?id=1P6wHM6HIEgJNysg6gWoHKuKDhBqXeIV-'
 export_file_name = 'export.pkl'
 
 classes = ['rino','indian_elephant', 'african_elephant']
